e3a991d8d7e25210adea2f772cc92a8681ac5e81
thornicek <tomas.hornicek8@email.cz>
2025-07-21 11:20:54 -0400

    Migrations added and Db updated

--- obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e21d63bb4dcdd83d3bf5e7f5bfea16ce23da259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+416b3c33de03b9cbeaa5eafc6bae4a1c53ef704e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
