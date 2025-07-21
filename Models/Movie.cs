2f0b6dc03d0af83bd1de5d51e7e03a4857dec4e8
thornicek <tomas.hornicek8@email.cz>
2025-07-21 13:25:52 -0400

    Added Data Validation, new Miggration + Db updated

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -7,12 +7,18 @@ public class Movie
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? Title { get; set; }
 
     public DateOnly ReleaseDate { get; set; }
 
+    [Required]
+    [StringLength(30)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$")]
     public string? Genre { get; set; }
 
+    [Range(0, 100)]
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
--- obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorWebAppMovies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a991d8d7e25210adea2f772cc92a8681ac5e81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ea91608f7cbae9f2dc34565a93f8b444ed6314")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
